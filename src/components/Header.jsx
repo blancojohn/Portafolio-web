1a5b97080633f11d42ffe34f1c16cbbf9e53f8f3
blancojohn <johnmario.bdc@gmail.com>
2025-09-16 14:19:04 -0300

    Redefiniendo nombre de clases y selectores

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,15 @@
 const Header = () => {
     return (
         <>
-            <header className="posicion-enlaces-navbar"> {/* Nombre de clase hace referencia área layout */}
+            <header className="links-navbar">
                 <nav>
-                    <a href="/">Experiencia</a>
-                    <a href="/">Proyectos</a>
-                    <a href="/">Sobre mí</a>
+                    <a className="link-b" href="/">Experiencia</a>
+                    <a className="link-b" href="/">Proyectos</a>
+                    <a className="link-b" href="/">Sobre mí</a>
                 </nav>
             </header>
-            <span className="posicion-enlace-formulario">
-                <a href="/" className="estilos-enlace-formulario">Contáctame</a>
+            <span className="posicion-link-formulario">
+                <a href="/" className="link-n">Contáctame</a>
             </span>
             <span className="posicion-logo">
                 <a href="/">LOGO</a>
