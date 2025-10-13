932011c215616f1be1826fc1d47e52b81e0cfa8f
blancojohn <johnmario.bdc@gmail.com>
2025-10-13 17:06:07 -0300

    Activación de la caja resaltadora de os links de navegación, deszplazamniento de la caja

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,36 @@
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 
 
 const Navbar = ({ navOpen }) => {
 
     const lastActiveLink = useRef();
-    const activeBox= useRef()
+    const activeBox = useRef()
+
+    const initActiveBox = () => {
+        activeBox.current.style.top = lastActiveLink.current.offsetTop + 'px';
+        activeBox.current.style.left = lastActiveLink.current.offsetLeft + 'px';
+        activeBox.current.style.width = lastActiveLink.current.offsetWidth + 'px';
+        activeBox.current.style.height = lastActiveLink.current.offsetHeight + 'px';
+    }
+
+    const activeCurrentLink = (event) => {
+        lastActiveLink.current?.classList.remove('active');
+        event.target.classList.add('active');
+        lastActiveLink.current = event.target;
+
+        activeBox.current.style.top = event.target.offsetTop + 'px';
+        activeBox.current.style.left = event.target.offsetLeft + 'px';
+        activeBox.current.style.width = event.target.offsetWidth + 'px';
+        activeBox.current.style.height = event.target.offsetHeight + 'px';
+    };
+
+
+    useEffect(initActiveBox, [])
 
     const dataForNavbar = [
         {
-            label: 'Inicio',
-            link: '#inicio',
+            label: 'Bienvenido',
+            link: '#bienvenido',
             className: 'Navbar-link--active',
             ref: lastActiveLink
         },
@@ -30,13 +51,13 @@ const Navbar = ({ navOpen }) => {
         }
     ];
 
-    let itemsNavbar= dataForNavbar.map(({ label, link, className, ref }, key) => (
+    let itemsNavbar = dataForNavbar.map(({ label, link, className, ref }, key) => (
         <a
             href={link}
             key={key}
             ref={ref}
             className={className}
-            onClick={null}
+            onClick={activeCurrentLink}
         >
             {label}
         </a>
@@ -49,8 +70,8 @@ const Navbar = ({ navOpen }) => {
                     itemsNavbar
                 }
                 <div
-                className="active-box"
-                ref={activeBox}
+                    className="active-box"
+                    ref={activeBox}
                 ></div>
             </nav>
         </>
