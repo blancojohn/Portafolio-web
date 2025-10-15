852edfdfc4770a403994e9cac6a4cbdf4128fad4
blancojohn <johnmario.bdc@gmail.com>
2025-10-15 13:29:39 -0300

    Componente Person: anexo archivo avatar, inicio estructura del componente

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,9 +1,59 @@
 
 
 
-const Person= ()=>{
+const Person = () => {
     return (
-        <h1>PERSON</h1>
+        <section
+            id="home"
+            className=""
+        >
+            <div className="">
+
+                <div>
+                    <div className="">
+                        <figure className="">
+                            <img
+                                src="/images/avatar-1.jpg"
+                                width={40}
+                                height={40}
+                                alt="John Blanco"
+                                className=""
+                            />
+                        </figure>
+                        <div className="">
+                            <span className="">
+                                <span className=""></span>
+                            </span>
+
+                            Disponible para trabajar
+                        </div>
+                    </div>
+
+                    <h2 className="">
+                        Building Scalable Modern Websites for the Future
+                    </h2>
+
+                    <div className="">
+                        ButtonPrimary
+
+                        ButonOutline
+                    </div>
+                </div>
+
+                <div className="">
+                    <figure className="">
+                        <img
+                            src="/images/jbfoto.png"
+                            width={656}
+                            height={800}
+                            alt="John Blanco"
+                            className=""
+                        />
+                    </figure>
+                </div>
+
+            </div>
+        </section>
     )
 }
 
